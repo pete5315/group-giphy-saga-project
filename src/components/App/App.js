5117667b27f47de6aa45040c7e1b95aa80b37866
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-15 11:42:14 -0500

    added routes to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import FavoritesView from '../Favorites/FavoritesView/FavoritesView';
 import Header from '../Header/Header';
 import SearchView from '../Search/SearchView/SearchView';
+import { HashRouter as Router, Route } from "react-router-dom";
+
 
 function App(props) {
   return (
     <div>
       <h1>Giphy Search!</h1>
       <Header/>
+      <Router>
+      <Route exact path="/">
       <SearchView/>
+      </Route>
+      <Route exact path="/favorites">
       <FavoritesView/>
+      </Route>
+      </Router>
     </div>
   );
 }
