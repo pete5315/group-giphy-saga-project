ce448f7cf64cd475ff2c1faa54445ef046c16679
Chris Clark <xchrisck@gmail.com>
2023-06-15 12:23:55 -0500

    completed the search bar Component

--- src/components/Search/SearchBar/SearchBar.jsx
+++ src/components/Search/SearchBar/SearchBar.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 
 function SearchBar(){
+    const dispatch = useDispatch();
+    const [searchQuery, setSearchQuery] = useState('');
 
+    const submitSearch = (value)=>{
+        dispatch({type: 'GET_GIFS', payload: value})
+    }
     return(
         <div>
-            Search Bar! 
+            <form onSubmit={()=>submitSearch(searchQuery)}>
+                <input onChange={(e)=>setSearchQuery(e.target.value)}/>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
