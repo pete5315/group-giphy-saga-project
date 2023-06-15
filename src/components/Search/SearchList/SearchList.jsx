cdcd45c70492ce12f8bf58219729a7f4f90fd58f
Majid Igale <majidigale18@gmail.com>
2023-06-15 15:21:41 -0500

    added index favsList reducer and created fetch route for FavList saga

--- src/components/Search/SearchList/SearchList.jsx
+++ src/components/Search/SearchList/SearchList.jsx
@@ -6,14 +6,14 @@ function SearchList(){
 
     const gifList = useSelector(store=>store.gifList);
     console.log('OUR GIF LIST =>>>>>',gifList);
-if(gifList.length >0)
+
     return(
         <div>
             SearchList
             {gifList.data ? (gifList.data.map((gif)=>(
                 <SearchListItem gif={gif}/>
             ))):(
-                <p>no Gifs to show</p>
+                <p>No Gifs to show</p>
             )}
             
         </div>
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import axios from 'axios';
 
 function* rootSaga() {
     yield takeLatest('GET_GIFS', fetchGifs)
+    yield takeLatest('GET_FAV', fetchFav)
 }
 
 function* fetchGifs(action) {
@@ -25,10 +26,29 @@ function* fetchGifs(action) {
     }
 }
 
+function* fetchFav() {
+
+    try {
+        const favResponse = yield axios.get('/api/favorite')
+        yield put ({
+            type: 'ADD_FAV',
+            payload: favResponse.data
+        })
+    } catch(err) {
+        console.log('Error with favsGET', err)
+    }
+}
 
 
 
-
+const favsList = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_FAV':
+            return action.payload;
+        default:
+            return state;
+    }
+    }
 
 
 
@@ -51,7 +71,7 @@ const gifList = (state = [], action) => {
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
-    combineReducers({gifList}),
+    combineReducers({gifList, favsList}),
     applyMiddleware(sagaMiddleware)
 )
 sagaMiddleware.run(rootSaga)
