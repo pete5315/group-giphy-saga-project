a119bd6832163e100e4718599e0666f6e1bbb249
Majid Igale <majidigale18@gmail.com>
2023-06-15 16:06:00 -0500

    added my old information

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,97 +1,97 @@
-// import * as React from "react";
-// import AppBar from "@mui/material/AppBar";
-// import Box from "@mui/material/Box";
-// import Toolbar from "@mui/material/Toolbar";
-// import Typography from "@mui/material/Typography";
-// import IconButton from "@mui/material/IconButton";
-// import MenuIcon from "@mui/icons-material/Menu";
-// import Drawer from "@mui/material/Drawer";
-// import List from "@mui/material/List";
-// import ListItem from "@mui/material/ListItem";
-// import ListItemButton from "@mui/material/ListItemButton";
-// import ListItemIcon from "@mui/material/ListItemIcon";
-// import ListItemText from "@mui/material/ListItemText";
-// import InboxIcon from "@mui/icons-material/MoveToInbox";
-// import MailIcon from "@mui/icons-material/Mail";
-// import { useHistory } from "react-router-dom";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import { useHistory } from "react-router-dom";
 
-// function Header() {
-//   // const history = useHistory();
-//   const [state, setState] = React.useState({
-//     top: false,
-//   });
+function Header() {
+  // const history = useHistory();
+  const [state, setState] = React.useState({
+    top: false,
+  });
 
-//   const toggleDrawer = (open) => (event) => {
-//     if (
-//       event.type === "keydown" &&
-//       (event.key === "Tab" || event.key === "Shift")
-//     ) {
-//       return;
-//     }
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
 
-//     setState({ ...state, top: open });
-//   };
+    setState({ ...state, top: open });
+  };
 
-//   const list = (anchor) => (
-//     <Box
-//       sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
-//       role="presentation"
-//       onClick={toggleDrawer(anchor, false)}
-//       onKeyDown={toggleDrawer(anchor, false)}
-//     >
-//       <List>
-//         {[
-//           ["Search", "/"],
-//           ["Favorites", "favorites"],
-//         ].map((text, index) => (
-//           <ListItem key={text[0]} disablePadding>
-//             <ListItemButton 
-//               // onClick={() => history.push(`/` + text[1])}
-//             >
-//               <ListItemIcon>
-//                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-//               </ListItemIcon>
-//               <ListItemText primary={text[0]} />
-//             </ListItemButton>
-//           </ListItem>
-//         ))}
-//       </List>
-//     </Box>
-//   );
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {[
+          ["Search", "/"],
+          ["Favorites", "favorites"],
+        ].map((text, index) => (
+          <ListItem key={text[0]} disablePadding>
+            <ListItemButton 
+              // onClick={() => history.push(`/` + text[1])}
+            >
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text[0]} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
-//   return (
-//     <>
-//       <Box sx={{ flexGrow: 1 }}>
-//         <AppBar position="static">
-//           <Toolbar>
-//             <IconButton
-//               size="large"
-//               edge="start"
-//               color="inherit"
-//               aria-label="menu"
-//               sx={{ mr: 2 }}
-//               onClick={toggleDrawer(!state.top)}
-//             >
-//               <MenuIcon />
-//               <Drawer
-//                 anchor={"top"}
-//                 open={state["top"]}
-//                 onClose={() => {
-//                   toggleDrawer(!state.top);
-//                 }}
-//               >
-//                 {list("top")}
-//               </Drawer>
-//             </IconButton>
-//             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-//               Current page
-//             </Typography>
-//           </Toolbar>
-//         </AppBar>
-//       </Box>
-//       <h2>WELCOME TO GIFSTAGRAM</h2>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={toggleDrawer(!state.top)}
+            >
+              <MenuIcon />
+              <Drawer
+                anchor={"top"}
+                open={state["top"]}
+                onClose={() => {
+                  toggleDrawer(!state.top);
+                }}
+              >
+                {list("top")}
+              </Drawer>
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Current page
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <h2>WELCOME TO GIFSTAGRAM</h2>
+    </>
+  );
+}
 
-// export default Header;
+export default Header;
